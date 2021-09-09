7cf539f5fdb25f9ec6f737c86f42f916fdfec69e
Francisco Serrano <8388391+FranciscoSerrano@users.noreply.github.com>
2021-09-09 10:07:53 -0400

    added plotly gauge

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 let promoterSlider = document.getElementById("promoters");
 let passiveSlider = document.getElementById("passives");
 let detractorSlider = document.getElementById("detractors");
@@ -5,6 +6,32 @@ let promotersValue = document.getElementById("promotersValue");
 let passivesValue = document.getElementById("passivesValue");
 let detractorsValue = document.getElementById("detractorsValue");
 let nps = document.getElementById("NPS");
+
+let data = [
+  {
+    domain: { x: [0, 1], y: [0, 1] },
+    value: 100,
+    title: { text: "NPS" },
+    type: "indicator",
+    mode: "gauge+number+delta",
+    delta: { reference: 70 },
+    gauge: {
+      axis: { range: [-100, 100] },
+      steps: [
+        { range: [0, 250], color: "white" },
+        { range: [250, 400], color: "gray" }
+      ],
+      threshold: {
+        line: { color: "red", width: 4 },
+        thickness: 0.75,
+        value: 70
+      }
+    }
+  }
+];
+
+let layout = { width: 600, height: 450, margin: { t: 0, b: 0 } };
+Plotly.newPlot('gauge', data, layout);
 
 promotersValue.innerHTML = promoterSlider.value; // Display the default slider value
 passivesValue.innerHTML = passiveSlider.value; // Display the default slider value
@@ -17,25 +44,16 @@ nps.innerHTML = calculateNPS(p, a, d);
 // Update the current slider value (each time you drag the slider handle)
 promoterSlider.oninput = function() {
   promotersValue.innerHTML = this.value;
-  p = parseInt(promoterSlider.value);
-  a = parseInt(passiveSlider.value);
-  d = parseInt(detractorSlider.value);
-  nps.innerHTML = calculateNPS(p, a, d);
+  updateNPS();
 }
 passiveSlider.oninput = function() {
   passivesValue.innerHTML = this.value;
-  p = parseInt(promoterSlider.value);
-  a = parseInt(passiveSlider.value);
-  d = parseInt(detractorSlider.value);
-  nps.innerHTML = calculateNPS(p, a, d);
-
+  updateNPS();
 }
+
 detractorSlider.oninput = function() {
   detractorsValue.innerHTML = this.value;
-  p = parseInt(promoterSlider.value);
-  a = parseInt(passiveSlider.value);
-  d = parseInt(detractorSlider.value);
-  nps.innerHTML = calculateNPS(p, a, d);
+  updateNPS();
 }
 
 
@@ -44,4 +62,13 @@ function calculateNPS(p, a, d) {
   const percentDetractor = ((d / (p + a + d)) * 100).toFixed(2);
   const NPS = percentPromoter - percentDetractor;
   return NPS.toFixed(2).toString();
+}
+
+function updateNPS() {
+  p = parseInt(promoterSlider.value);
+  a = parseInt(passiveSlider.value);
+  d = parseInt(detractorSlider.value);
+  nps.innerHTML = calculateNPS(p, a, d);
+  data[0].value = parseInt(nps.innerHTML).toFixed(2);
+  Plotly.newPlot('gauge', data, layout);
 }
