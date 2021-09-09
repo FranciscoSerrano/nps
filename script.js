0fa793ae81ff01d738854d8f63f2b1cb5a525227
Francisco Serrano <8388391+FranciscoSerrano@users.noreply.github.com>
2021-09-09 08:23:30 -0400

    fixed NPS calculations

--- script.js
+++ script.js
@@ -9,35 +9,39 @@ let nps = document.getElementById("NPS");
 promotersValue.innerHTML = promoterSlider.value; // Display the default slider value
 passivesValue.innerHTML = passiveSlider.value; // Display the default slider value
 detractorsValue.innerHTML = detractorSlider.value; // Display the default slider value
-let p = promoterSlider.value;
-let a = passiveSlider.value;
-let d = detractorSlider.value;
-nps.innerHTML = calculateNPS(p, a, d)
+
+let p = parseInt(promoterSlider.value);
+let a = parseInt(passiveSlider.value);
+let d = parseInt(detractorSlider.value);
+nps.innerHTML = calculateNPS(p, a, d);
 // Update the current slider value (each time you drag the slider handle)
 promoterSlider.oninput = function() {
   promotersValue.innerHTML = this.value;
-  p = promoterSlider.value;
-  a = passiveSlider.value;
-  d = detractorSlider.value;
+  p = parseInt(promoterSlider.value);
+  a = parseInt(passiveSlider.value);
+  d = parseInt(detractorSlider.value);
   nps.innerHTML = calculateNPS(p, a, d);
 }
 passiveSlider.oninput = function() {
   passivesValue.innerHTML = this.value;
-  p = promoterSlider.value;
-  a = passiveSlider.value;
-  d = detractorSlider.value;
+  p = parseInt(promoterSlider.value);
+  a = parseInt(passiveSlider.value);
+  d = parseInt(detractorSlider.value);
   nps.innerHTML = calculateNPS(p, a, d);
 
 }
 detractorSlider.oninput = function() {
   detractorsValue.innerHTML = this.value;
-  p = promoterSlider.value;
-  a = passiveSlider.value;
-  d = detractorSlider.value;
+  p = parseInt(promoterSlider.value);
+  a = parseInt(passiveSlider.value);
+  d = parseInt(detractorSlider.value);
   nps.innerHTML = calculateNPS(p, a, d);
 }
 
 
 function calculateNPS(p, a, d) {
-  return ((p - d) / (p + a + d)) * 100;
+  const percentPromoter = ((p / (p + a + d)) * 100).toFixed(2);
+  const percentDetractor = ((d / (p + a + d)) * 100).toFixed(2);
+  const NPS = percentPromoter - percentDetractor;
+  return NPS.toFixed(2).toString();
 }
